21ec9c6150546801535774d2661c11ff4050108a
The-Infinity-Glitch <theinfinityglitch@gmail.com>
2024-09-05 20:45:18 -0300

    Rewrited all things on rust

---
+++ cli/src/main.rs
@@ -0,0 +1,25 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    parse_args(&args);
+}
+
+fn parse_args(args: &Vec<String>) {
+    if args.len() < 2 {
+        help_content();
+    } else {
+        for arg in args {    
+            if arg.chars().nth(0).unwrap() == '-' {
+                println!("{}: argument.", arg);
+            } else {
+                println!("{}: source file.", arg);
+            }
+        }
+    }
+}
+
+// TODO: Make a better help mensage for the users
+fn help_content() {
+    println!("Help content comming soon!");
+}
