0bf7b88c526908f46c54da1946d90a750719c1d3
The-Infinity-Glitch <theinfinityglitch@gmail.com>
2024-09-05 22:54:15 -0300

    Compiler flags set from cli

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,17 +1,30 @@
 use std::env;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    parse_args(&args);
+struct Flags {
+    werror: bool
 }
 
-fn parse_args(args: &Vec<String>) {
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let mut compiler_flags = Flags {
+        werror: false
+    };
+    
+    parse_args(&args, &mut compiler_flags);
+}
+
+fn parse_args(args: &Vec<String>, flags: &mut Flags) {
     if args.len() < 2 {
         help_content();
     } else {
         for arg in args {    
             if arg.chars().nth(0).unwrap() == '-' {
-                println!("{}: argument.", arg);
+                match arg.as_str() {
+                    "-werror" => flags.werror = true,
+                    _ => println!("{}: Non existent argument", arg)
+                }
             } else {
                 println!("{}: source file.", arg);
             }
