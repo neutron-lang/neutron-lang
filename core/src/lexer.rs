2529ad45d7f6c8e79c11c6a84ba4083236a1b9e8
The-Infinity-Glitch <theinfinityglitch@gmail.com>
2024-09-07 20:52:02 -0300

    Tokenizer work in progress

--- core/src/lexer.rs
+++ core/src/lexer.rs
@@ -1,12 +1,71 @@
-//enum Token {
-//    SEMICOLON
-//}
+use colored::Colorize;
+
+// All tokens of the language
+enum Token {
+    // Special symbols.
+    LeftParenthesis,
+    RightParenthesis,
+    LeftBrace,
+    RightBrace,
+    LeftBracket,
+    RightBracket,
+    LeftArrow,
+    RightArrow,
+    Dot,
+    Comma,
+    DoubleDot,
+    Column,
+    SemiColumn,
+    StartComment,
+    EndComment,
+    Space,
+    
+    // Simple operators and mixed operators.
+    Operator(String),
+    
+    // Indentifiers, like: names of functions, variables...
+    Indentifier(String),
+    
+    // Data types.
+    String(String),
+    Intenger(i64),
+    Float(f64),
+    Bool(bool),
+    Char(char),
+    
+    // Tokenizer error
+    Error
+}
 
 pub fn lexer_source(source: &String) {
     for a in source_to_array(&source) {
-        match &a.to_string().as_str() {
-            &"import" => println!("Import Token"),
-            _ => println!("Nothing")
+        let result = tokenizer(&a);
+        
+        match result {
+            Token::LeftParenthesis => println!("Works"),
+            Token::RightParenthesis => todo!(),
+            Token::LeftBrace => todo!(),
+            Token::RightBrace => todo!(),
+            Token::LeftBracket => todo!(),
+            Token::RightBracket => todo!(),
+            Token::LeftArrow => todo!(),
+            Token::RightArrow => todo!(),
+            Token::Dot => todo!(),
+            Token::Comma => todo!(),
+            Token::DoubleDot => todo!(),
+            Token::Column => todo!(),
+            Token::SemiColumn => todo!(),
+            Token::StartComment => todo!(),
+            Token::EndComment => todo!(),
+            Token::Space => todo!(),
+            Token::Operator(_) => todo!(),
+            Token::Indentifier(_) => todo!(),
+            Token::String(_) => todo!(),
+            Token::Intenger(_) => todo!(),
+            Token::Float(_) => todo!(),
+            Token::Bool(_) => todo!(),
+            Token::Char(_) => todo!(),
+            Token::Error => println!("{}: In progress", "Error".red()),
         }
     }
 }
@@ -41,4 +100,12 @@ fn source_to_array(source: &String) -> Vec<String> {
     }
     
     return result_array;
+}
+
+fn tokenizer(input: &String) -> Token {
+    match input.as_str() {
+        "(" => return Token::LeftParenthesis,
+        ")" => return Token::RightParenthesis,
+         _  => return Token::Error
+    }
 }
