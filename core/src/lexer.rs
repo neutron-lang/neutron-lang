f271bb92cdc0fd92830c66a5e0f16c7d119b217d
The-Infinity-Glitch <theinfinityglitch@gmail.com>
2024-09-08 12:28:02 -0300

    Now the lexer support binary operators, but the tokens of binary operators needs to be implemented.

--- core/src/lexer.rs
+++ core/src/lexer.rs
@@ -25,15 +25,15 @@ pub enum Token {
 }
 
 pub fn lexer_source(source: &String) -> Vec<Token> {
-    let mut token_array = vec![];
+    let mut token_vector = vec![];
     
-    for a in source_to_array(&source) {
+    for a in source_to_vector(&source) {
         let result = tokenizer(&a);
         
-        token_array.insert(token_array.len(), result);
+        token_vector.insert(token_vector.len(), result);
     }
     
-    for t in &token_array {
+    for t in &token_vector {
         match t {
             Token::LeftParenthesis => println!("LeftParenthesis"),
             Token::RightParenthesis => println!("RightParenthesis"),
@@ -48,33 +48,34 @@ pub fn lexer_source(source: &String) -> Vec<Token> {
             Token::Column => println!("Column"),
             Token::SemiColumn => println!("SemiColumn"),
             Token::Space => println!("Space"),
-            Token::Operator(_) => println!("Operator"),
-            Token::Indentifier(_) => println!("Indentifier"),
+            Token::Operator(value) => println!("Operator({value})"),
+            Token::Indentifier(value) => println!("Indentifier({value})"),
         }
     }
     
-    return token_array;
+    return token_vector;
 }
 
-fn source_to_array(source: &String) -> Vec<String> {
+// Transform the source into a string vector
+fn source_to_vector(source: &String) -> Vec<String> {
     // All the symbols and operators used for mark the division of the words
     let special_symbols = vec!["(", ")", "{", "}", ";", ",", ".", r#"""#];
     let operators = vec!["=", "+", "-", "*", "/", "<", ">"];
     
     let mut current_text = String::new(); // current_text: retains the current word formed by the c in the for loop below.
-    let mut result_array = vec![];   // result_array: retains the result of the function, the for loop below insert current_text here before current_text clear.
+    let mut result_vector = vec![];   // result_vector: retains the result of the function, the for loop below insert current_text here before current_text clear.
 
-    // The c variable of the loop retains the current char of the source, if it's a special symbol or operator, it's inserted in result_array, else, it is inserted in current_word.
+    // The c variable of the loop retains the current char of the source, if it's a special symbol or operator, it's inserted in result_vector, else, it is inserted in current_word.
     for c in source.chars() {
         if c == ' ' || c == '\n' || special_symbols.contains(&c.to_string().as_str()) || operators.contains(&c.to_string().as_str()) {
-            // If current_text is diferent of nothing, so insert it in result_array.
+            // If current_text is diferent of nothing, so insert it in result_vector.
             if current_text != ""{
-                result_array.insert(result_array.len(), current_text.clone());
+                result_vector.insert(result_vector.len(), current_text.clone());
             }
             
             // If the current char is a \n, ignore it, else insert it in current_array
             if c != '\n' {
-                result_array.insert(result_array.len(), c.to_string());
+                result_vector.insert(result_vector.len(), c.to_string());
             }
             
             // Clear current_text
@@ -85,30 +86,48 @@ fn source_to_array(source: &String) -> Vec<String> {
         }
     }
     
-    correct_array(&result_array);
-    
-    return result_array;
+    return correct_vector(&result_vector);
 }
 
-fn correct_array(input: &Vec<String>) -> Vec<String> {
+// Find binary operators separated in the vector, and transform in a single vector unit.
+fn correct_vector(input: &Vec<String>) -> Vec<String> {
     let mut result = vec![];
     let operators = vec!["=", "+", "-", "*", "/", "<", ">"];
+    let mut skip_value: bool = false;
     
-    for (index, &value) in input.iter().enumerate() {
-        if operators.contains(&value.as_str()) {
-            if operators.contains() {
-                result.insert(result.len(), value.to_string());
+    for (index, value) in input.iter().enumerate() {
+        if skip_value != true {
+            if operators.contains(&value.as_str()) {
+                let next_opt = input.get(index + 1);
+                let mut next = String::new();
+                
+                match next_opt {
+                    Some(v) => next.push_str(v),
+                    _ => next.push_str("null")
+                }
+                
+                if operators.contains(&next.as_str()) {
+                    let binary_operator = value.to_string() + next.as_str();
+                    
+                    result.insert(result.len(), binary_operator);
+                    
+                    skip_value = true;
+                } else {
+                    result.insert(result.len(), value.to_string());
+                    skip_value = false;
+                }
             } else {
                 result.insert(result.len(), value.to_string());
             }
         } else {
-            result.insert(result.len(), value.to_string());
+            skip_value = false;
         }
     }
-    
+        
     return result;
 }
 
+// Receive a string and transform it in a Token.
 fn tokenizer(input: &String) -> Token {
     match input.as_str() {
         "(" => return Token::LeftParenthesis,
--- tests/if_else.sol
+++ tests/if_else.sol
@@ -3,7 +3,7 @@ import std;
 fun main() {
     var test = 1;
 
-    if test == 1 {
-        print("aiin\n");
+    if test==1 {
+        print("true\n");
     }
 }
