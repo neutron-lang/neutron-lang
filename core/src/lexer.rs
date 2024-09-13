2de2fcf18022c4e6eecf60aacb7b2be60ba47c9c
The-Infinity-Glitch <theinfinityglitch@gmail.com>
2024-09-12 23:29:07 -0300

    Added some tokens to the new lexer.

--- core/src/lexer.rs
+++ core/src/lexer.rs
@@ -3,53 +3,71 @@ use logos::{Lexer, Logos};
 // All tokens of the language
 #[derive(Logos, Debug)]
 #[logos(extras = (usize, usize))]
-pub enum Token {
+pub enum TokenType {
+    #[token("(")]
+    LeftParenthesis,
+    #[token(")")]
+    RigthParenthesis,
+    
+    #[token("[")]
+    LeftBrace,
+    #[token("]")]
+    RigthBrace,
+    
+    #[token("{")]
+    LeftBracket,
+    #[token("}")]
+    RigthBracket,
+    
+    #[token(".")]
+    Period,
+    
+    #[token(",")]
+    Comma,
+    
+    #[token(":")]
+    Column,
+    
+    #[token(";")]
+    SemiColumn,
+    
+    #[token(r#"'"#)]
+    SingleQuote,
+    #[token(r#"""#)]
+    DoubleQuotes,
+    
     #[regex(r"\n", newline_callback)]
     NewLine,
     
+    #[token(" ")]
+    Space,
+    
     #[token("import", word_callback)]
     #[token("func", word_callback)]
+    #[token("var", word_callback)]
     Keyword((usize, usize)),
     
     #[regex("[a-zA-Z]+", word_callback)]
     Identifier((usize, usize)),
 }
 
-/*
-// A struct that armazenates the token value and his type.
-#[derive(Debug)]
-pub struct Token {
-    value: String,
-    token_type: TokenType
-}
-*/
-
-fn newline_callback(lex: &mut Lexer<Token>) {
+fn newline_callback(lex: &mut Lexer<TokenType>) {
     lex.extras.0 += 1;
     lex.extras.1 = lex.span().end;
 }
 
 /// Compute the line and column position for the current word.
-fn word_callback(lex: &mut Lexer<Token>) -> (usize, usize) {
+fn word_callback(lex: &mut Lexer<TokenType>) -> (usize, usize) {
     let line = lex.extras.0;
     let column = lex.span().start - lex.extras.1;
 
     (line, column)
 }
 
-pub fn lex_source(source: &String) {
-    let mut lex = Token::lexer(source);
+pub fn lex_source(source: &String){
+    let mut lex = TokenType::lexer(source);
 
     while let Some(token) = lex.next() {
-        match token {
-            Ok(tp) => {
-                match tp {
-                    Token::NewLine => println!("Newline found"),
-                    Token::Keyword((line, column)) => println!("Keyword '{}' found at ({}, {})", lex.slice(), line, column),
-                    Token::Identifier((line, column)) => println!("Indetifier '{}' found at ({}, {})", lex.slice(), line, column)
-                }
-            },
-            Err(e) => println!("Error: {:?}", e)
-        }
+        dbg!(&token);
     }
 }
